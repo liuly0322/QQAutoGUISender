2218c4042be1ba93048d4cf26d2e3253081f16ac
liuly <me@liuly.moe>
2024-02-14 16:32:47 +0800

    feat: more error hint & timeout

--- image_handler.py
+++ image_handler.py
@@ -1,5 +1,6 @@
 import re
 import os
+import time
 import requests
 
 os.makedirs("tmp", exist_ok=True)
@@ -8,15 +9,28 @@ class ImageHandler:
     @staticmethod
     def download_image(url, filename):
         response = requests.get(url, stream=True, timeout=(4, 4))
-        if response.status_code != 200 or \
-            not response.headers['Content-Type'].startswith('image') or \
-            int(response.headers['Content-Length']) > 50 * 1024 * 1024:
-            return False
+        if response.status_code != 200:
+            raise ValueError(f"下载失败喵: {response.status_code}")
+
+        size = 0
+        maximum_size = 1024 * 1024 * 50
+        start = time.time()
+        receive_timeout = 4
+
+        def write_chunk(file, chunk):
+            nonlocal size
+            size += len(chunk)
+            if size > maximum_size:
+                raise ValueError("图片太大了喵")
+
+            if time.time() - start > receive_timeout:
+                raise TimeoutError("下载超时喵")
+
+            file.write(chunk)
 
         with open(filename, 'wb') as file:
             for chunk in response.iter_content(chunk_size=1024):
-                file.write(chunk)
-        return True
+                write_chunk(file, chunk)
 
     @staticmethod
     def handle_content(content: str):
@@ -26,8 +40,8 @@ class ImageHandler:
             for i, match in enumerate(matches):
                 picture_url_path = match.group("url")
                 picture_path = os.path.join("tmp", f"{i}")
-                if ImageHandler.download_image(picture_url_path, picture_path):
-                    picture_paths += [picture_path]
-                    content = content.replace(match.group("full"), f"[image: {i}]")
+                ImageHandler.download_image(picture_url_path, picture_path)
+                picture_paths += [picture_path]
+                content = content.replace(match.group("full"), f"[image: {i}]")
         return content, picture_paths
 
