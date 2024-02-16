ae13adddaf9096aef6afe1e5476f633b12fd0fb1
liuly <me@liuly.moe>
2024-02-16 18:47:10 +0800

    feat: allow send to multi groups

--- message_sender.py
+++ message_sender.py
@@ -12,7 +12,28 @@ class MessageSender:
             exit(1)
         self.text_box_x, self.text_box_y = pyautogui.center(text_box_position)
 
+        search_box_position = pyautogui.locateOnScreen('asset/search_box.png')
+        if not search_box_position:
+            print("无法找到搜索框")
+            exit(1)
+        self.search_box_x, self.search_box_y = pyautogui.center(search_box_position)
+
+        self.current_group_id = None
+
+    def switch_group(self, group_id) -> None:
+        if group_id == self.current_group_id:
+            return
+
+        pyautogui.click(self.search_box_x, self.search_box_y)
+        time.sleep(0.1)
+        pyautogui.write(group_id)
+        pyautogui.press('enter')
+        time.sleep(0.1)
+
+        self.current_group_id = group_id
+
     def send(self, group_id, message, picture_paths=None, at_numbers=None) -> None:
+        self.switch_group(group_id)
         pyautogui.click(self.text_box_x, self.text_box_y)
 
         pyperclip.copy(message)
