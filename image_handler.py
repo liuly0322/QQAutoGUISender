48d595e172e8192fac8b81117b531ddcb2894624
Momo-Tori <813424850@qq.com>
2024-09-17 22:25:12 +0800

    feat: change sender to onebot-11 protocol

--- image_handler.py
+++ image_handler.py
@@ -7,55 +7,26 @@ import requests
 
 os.makedirs("tmp", exist_ok=True)
 
+
 class ImageHandler:
-    @staticmethod
-    def download_image(url, filename):
-        response = requests.get(url, stream=True, timeout=(4, 4))
-        if response.status_code != 200:
-            raise ValueError(f"下载失败喵: {response.status_code}")
-
-        size = 0
-        maximum_size = 1024 * 1024 * 50
-        start = time.time()
-        receive_timeout = 20
-
-        def write_chunk(file, chunk):
-            nonlocal size
-            size += len(chunk)
-            if size > maximum_size:
-                raise ValueError("图片太大了喵")
-
-            if time.time() - start > receive_timeout:
-                raise TimeoutError("下载超时喵")
-
-            file.write(chunk)
-
-        with open(filename, 'wb') as file:
-            for chunk in response.iter_content(chunk_size=1024):
-                write_chunk(file, chunk)
 
     @staticmethod
     def replace_image(content: str):
         matches = re.compile(r'(?P<full>!\[\]\((?P<url>.*?)\))').finditer(content)
-        picture_paths = []
         if matches:
             for i, match in enumerate(matches):
                 picture_url_path = match.group("url")
-                picture_path = os.path.join("tmp", f"{i}")
-                ImageHandler.download_image(picture_url_path, picture_path)
-                picture_paths += [picture_path]
-                content = content.replace(match.group("full"), f"[image: {i}]")
-        return content, picture_paths
+                content = content.replace(
+                    match.group("full"), f"[CQ:image,file={picture_url_path}]"
+                )
+        return content
 
     @staticmethod
-    def add_user_upload_image(content: str, picture_paths: List[str], image: Optional[str]):
+    def add_user_upload_image(content: str, image: Optional[str]):
         if not image:
-            return content, picture_paths
+            return content
 
-        image_path = os.path.join("tmp", f"{len(picture_paths)}")
-        image_file_content = b64decode(image.split(",")[1])
-        with open(image_path, "wb") as file:
-            file.write(image_file_content)
+        image = image.split(",")[1]
 
-        content = content + "[upload_image]"
-        return content, picture_paths + [image_path]
+        content = content + f"[CQ:image,file=base64://{image}]"
+        return content
--- message_sender.py
+++ message_sender.py
@@ -1,60 +1,14 @@
-import pyautogui
-import pyperclip
-import time
-import subprocess
+import requests
 
 
 class MessageSender:
-    def __init__(self) -> None:
-        text_box_position = pyautogui.locateOnScreen('asset/text_box.png')
-        self.text_box_x, self.text_box_y = pyautogui.center(text_box_position)
 
-        search_box_position = pyautogui.locateOnScreen('asset/search_box.png')
-        self.search_box_x, self.search_box_y = pyautogui.center(search_box_position)
+    def __init__(self, url) -> None:
+        self.url = url + "/send_group_msg"
 
-        self.current_group_id = None
-
-    def switch_group(self, group_id) -> None:
-        if group_id == self.current_group_id:
-            return
-
-        pyautogui.click(self.search_box_x, self.search_box_y)
-        time.sleep(0.1)
-        pyperclip.copy(group_id)
-        time.sleep(1)
-        pyautogui.hotkey('ctrl', 'v')
-        time.sleep(1)
-        pyautogui.press('enter')
-        time.sleep(0.1)
-
-        self.current_group_id = group_id
-
-    def send(self, group_id, message, picture_paths=None, at_numbers=None) -> None:
-        self.switch_group(group_id)
-        pyautogui.click(self.text_box_x, self.text_box_y)
-
-        pyperclip.copy(message)
-        time.sleep(1)
-        pyautogui.hotkey('ctrl', 'v')
-
-        for picture_path in picture_paths or []:
-            subprocess.run(
-                ["xclip", "-selection", "clipboard", "-t", "image/png", "-i", f"{picture_path}"]
-            )
-            time.sleep(2)
-            pyautogui.hotkey('ctrl', 'v')
-            time.sleep(1)
-
-        for at_number in at_numbers or []:
-            pyautogui.write(f"@{at_number}")
-            pyautogui.press('enter')
-            time.sleep(1)
-
-        pyautogui.press(['esc', 'enter'])
-
-
-if __name__ == "__main__":
-    sender = MessageSender()
-    sender.send("hello!")
-    sender.send("hello.png: ", ["asset/hello.png", "asset/hello.png"])
-    sender.send("这里是自动发送的消息。")
+    def send(self, group_id, message) -> None:
+        post_json = {
+            "group_id": group_id,
+            "message": message,
+        }
+        response = requests.post(f"http://{self.url}", json=post_json)
